aa52b638ab43bdd6b6b6088e71afdae2ec2d31a3
Natchapon Srisuk <natchapon.18851@gmail.com>
2026-08-14 21:33:40 +0700

    rig_to_anim validate py file

--- PublishValidation/rig_publisher/rig_to_anim_validate.py
+++ PublishValidation/rig_publisher/rig_to_anim_validate.py
@@ -5,7 +5,23 @@ import maya.cmds as cmds
 
 from tmlib.core import Scene
 
-def validate():
+
+import os
+from UkoreMaya.core import Pipeline
+
+def validate(context):
     result = Scene.clean_up_scene()
-    return result
+
+    if result == False:
+        return False
+    
+    folder_name = context["ticket"]["folder_name"]
+    version = context["version"]
+    ma_path = os.path.join(context["version_dir"], "{}_v{:03d}.ma".format(folder_name, version))
+    Pipeline.export_maya_common(export_file_path=ma_path)
+
+    print("Exported Maya Ascii file to: {}".format(ma_path)
+          )
+    
+    return True
 
